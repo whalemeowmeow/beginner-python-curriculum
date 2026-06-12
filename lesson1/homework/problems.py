223c90702e83ebceae5df628d41fef02d0a9385d
whalemeowmeow <revanw.zixuan@gmail.com>
2026-06-11 21:27:49 -0700

    Update problems.py

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,17 +1,17 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
-
+luck = 7
+print (luck)
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
-
+sleep_time = 8-9
+print (sleep_time)
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
-
+fruit = pear
+print (my favorite fruit is , fruit)
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
