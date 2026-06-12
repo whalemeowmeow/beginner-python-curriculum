d4d18be03a38a793c739ba883237c835e4ad05eb
whalemeowmeow <revanw.zixuan@gmail.com>
2026-06-11 21:37:38 -0700

    Update problems.py

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -11,14 +11,17 @@ print (sleep_time)
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
 fruit = pear
-print (my favorite fruit is , fruit)
+print ("my favorite fruit is" , fruit)
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
-
-
+city = seattle
+conutry = America
+print (city)
+print (conutry)
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+pet = kitty cat
+age_pet = 1 year
